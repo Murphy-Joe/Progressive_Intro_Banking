5f77317138ea16d479356288ea216867021f7b2e
Jeffry Gonzalez <jeff@hypertheory.com>
2021-11-10 15:19:24 -0500

    done with the clock stuff

---
+++ BankingDomain/StandardBusinessClock.cs
@@ -0,0 +1,17 @@
+﻿namespace BankingDomain
+{
+    public class StandardBusinessClock : IProvideTheBusinessClock
+    {
+        private readonly ISystemTime _systemTime;
+
+        public StandardBusinessClock(ISystemTime systemTime)
+        {
+            _systemTime = systemTime;
+        }
+
+        public bool IsDuringBusinessHours()
+        {
+            return (_systemTime.GetCurrent().Hour >= 9 && _systemTime.GetCurrent().Hour < 17);
+        }
+    }
+}
---
+++ BankingUnitTests/StandardBusinessClockTests/ReturnsCorrectBusinessOpeningStatus.cs
@@ -0,0 +1,38 @@
+﻿using BankingDomain;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankingUnitTests.StandardBusinessClockTests
+{
+    public class ReturnsCorrectBusinessOpeningStatus
+    {
+        [Fact]
+        public void Between9And5IsOpen()
+        {
+            var stubbedClock = new Mock<ISystemTime>();
+            stubbedClock.Setup(c => c.GetCurrent())
+                .Returns(new DateTime(1969, 4, 20, 11, 39, 00));
+            IProvideTheBusinessClock clock = new StandardBusinessClock(stubbedClock.Object);
+
+
+            Assert.True(clock.IsDuringBusinessHours());
+        }
+
+        [Fact]
+        public void OtherwiseClosed()
+        {
+            var stubbedClock = new Mock<ISystemTime>();
+            stubbedClock.Setup(c => c.GetCurrent())
+                 .Returns(new DateTime(1969, 4, 20, 18, 39, 00));
+            IProvideTheBusinessClock clock = new StandardBusinessClock(stubbedClock.Object);
+      
+
+            Assert.False(clock.IsDuringBusinessHours());
+        }
+    }
+}
